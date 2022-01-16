29f9fbdb40b167df5fa82719dd236b53fc01e815
Jeronimo Mendes <jeronimo.m.mendes@gmail.com>
2022-01-16 20:01:31 +0000

    Reduce number of `relations`

--- api/models.py
+++ api/models.py
@@ -5,21 +5,30 @@ from django.db.models.fields.related import ForeignKey
 class Group(models.Model):
     name = models.CharField(max_length=30, default="Group")
     users = models.ManyToManyField("Profile")
-    relations = models.ManyToManyField("Relation")
     movements = models.ManyToManyField("Movement")
 
     def add_movement(self, amount, creator, description):
         amount /= users.count()
         movement = Movement.objects.create(amount=abs(amount), author=creator, description=description)
-        for relation in self.relations:
-            if relation.has_user(creator):
-                relation.add_movement(amount, creator, movement, False)
+        for user in self.users:
+            if user == creator: 
+                continue
+            
+            # fetch relation between users
+            # there must be a better way of doing this
+            # TODO: filter with an "or" (user1 == user or user2 == user)
+            relation = creator.relations.objects.filter(user1=user)
+            if not relation:
+                creator.relations.objects.filter(user2=user)
+
+            relation.add_movement(amount, creator, movement, False)
 
         self.movements.add(movement)
 
 class Profile(models.Model):
     email_notifications = models.BooleanField(default=True)
     groups = models.ManyToManyField("Group")
+    relations = models.ManyToManyField("Relation")
 
 class Movement(models.Model):
     date = models.DateField(auto_now_add=True)

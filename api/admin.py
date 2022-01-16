9f9dc961a9d37e27ba80d32a8f100af441fb3bf6
Jeronimo Mendes <jeronimo.m.mendes@gmail.com>
2022-01-16 20:03:29 +0000

    Add models to admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Relation)
+admin.site.register(Movement)
+admin.site.register(Group)

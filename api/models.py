0d6f06c436eead6d891e136ecf74be80d652848c
Jeronimo Mendes <jeronimo.m.mendes@gmail.com>
2022-01-16 20:05:13 +0000

    Fix imports

--- api/models.py
+++ api/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
 from django.db.models.fields.related import ForeignKey
 
 

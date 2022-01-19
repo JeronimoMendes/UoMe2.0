3f200b7a77e80d998956ff1cd6c332ee41b168bf
Jeronimo Mendes <jeronimo.m.mendes@gmail.com>
2022-01-19 00:40:18 +0000

    Add `User` serializer

--- api/serializer.py
+++ api/serializer.py
@@ -1,8 +1,15 @@
 from .models import *
 from django.db.models import fields
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
 class MovementSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Movement
 		fields = ("__all__")
+
+class UserSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = User
+		fields = ("email")
+
